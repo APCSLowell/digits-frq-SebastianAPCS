6486b539e5ddf5df8d5e568707f14f38405ac5bb
Sebastian Dowell <142616573+SebastianAPCS@users.noreply.github.com>
2024-04-17 20:20:16 +0000

    completed Digits class

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
 
-public class Digits
-{
+public class Digits {
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	public Digits(int num) {
+		digitList = new ArrayList<>();
+
+		if (num == 0) {
+			digitList.add(0);
+		} else {
+			while (num > 0) {
+				digitList.add(0, num % 10);
+				num /= 10;
+			}
+		}
 	}
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
+	public boolean isStrictlyIncreasing() {
+		for (int i = 0; i < digitList.size() - 1; i++) {
+			if (digitList.get(i) >= digitList.get(i + 1)) return false;
+		}
 
+		return true;
 	}
 	
-	public String toString()
-	{
+	public String toString() {
 		return digitList.toString();
 	}
 }
